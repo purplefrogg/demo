0d41e9b1e70433559ec8f55eac4e80b1eef68258
purplefrogg <purplefroggy23@gmail.com>
2021-01-08 23:23:23 +0500

    style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="wrapper">
+		<div className="header">
+			<div className="header__logo"></div>
+			<div className="header__menu">
+				
+			</div>
+		</div>
+			
+		<div className="body">
+			<div className="navbar">
+				<div className="navbar__list">
+					<div className="navbar__item">
+						
+						<div className="item__label">News</div>
+					</div>
+					<div className="navbar__item">
+						
+						<div className="item__label">messenger</div>
+					</div>
+					
+				</div>
+			</div>
+			<div className="content"></div>
+		</div>
+		
+	</div>
     </div>
   );
 }
