4ac0090ee539fb62f382d0a11de2955a825b0853
purplefrogg <purplefroggy23@gmail.com>
2021-01-09 23:02:41 +0500

    grid

--- src/App.js
+++ src/App.js
@@ -4,33 +4,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="wrapper">
-		<div className="header">
-			<div className="header__logo"></div>
-			<div className="header__menu">
-				
-			</div>
-		</div>
-			
-		<div className="body">
+    <div className="wrapper">
+      <div className="header">
+        <div className="header__logo"> </div>
+      </div>
+			<div className="header__menu"></div>
+      <div className="body">
 			<div className="navbar">
 				<div className="navbar__list">
 					<div className="navbar__item">
-						
-						<div className="item__label">News</div>
+					
+						<div className="item__label">News
+            </div>
 					</div>
 					<div className="navbar__item">
-						
-						<div className="item__label">messenger</div>
+						<div className="item__label">messenger
+            </div>
 					</div>
-					
+					<div className="navbar__item">
+			
+						<div className="item__label">friends
+            </div>
+					</div>
+					<div className="navbar__item">
+						<div className="item__label">profile
+            </div>
+					</div>
 				</div>
 			</div>
 			<div className="content"></div>
 		</div>
-		
+		</div>		
+	
 	</div>
-    </div>
+
   );
 }
 
