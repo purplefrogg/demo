cdec317f25b2c7b363c1556f69c6c7079c0d769a
purplefrogg <purplefroggy23@gmail.com>
2021-01-13 22:24:45 +0500

    12 lesson

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
+import React from 'react';
+import './App.scss';
+import Body from './components/Body';
+import Header from './components/Header';
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-    <div className="wrapper">
-      <div className="header">
-        <div className="header__logo"> </div>
-      </div>
-			<div className="header__menu"></div>
-      <div className="body">
-			<div className="navbar">
-				<div className="navbar__list">
-					<div className="navbar__item">
-					
-						<div className="item__label">News
-            </div>
-					</div>
-					<div className="navbar__item">
-						<div className="item__label">messenger
-            </div>
-					</div>
-					<div className="navbar__item">
-			
-						<div className="item__label">friends
-            </div>
-					</div>
-					<div className="navbar__item">
-						<div className="item__label">profile
-            </div>
-					</div>
-				</div>
+const App = () => {
+	return (
+		<div className="App">
+			<div className="wrapper">
+				<Header />
+				<Body />
 			</div>
-			<div className="content"></div>
-		</div>
-		</div>		
-	
-	</div>
 
-  );
+		</div>
+
+	);
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+        <div className="header__logo"> </div>
+		<div className="header__menu"></div>
+      </div>
+    )
+}
+
+export default Header;
