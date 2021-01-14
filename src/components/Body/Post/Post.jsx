8f625c4207f56e25e87ac4ddc1cf1758f7466694
purplefrogg <purplefroggy23@gmail.com>
2021-01-14 23:06:23 +0500

    16: props

--- src/components/Body/Post/Post.jsx
+++ src/components/Body/Post/Post.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import style from './Post.module.scss'
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={style.post}>
             <div className={style.header}>
-                <div className={style.image}>
-                    <img src="https://meragor.com/files/styles/ava_800_800_wm/ava-251.jpg" alt=""/>
+                <div className={style.icon}>
+                    <img src="https://meragor.com/files/styles/ava_800_800_wm/ava-251.jpg" alt="" />
                 </div>
                 <div className={style.author}>anime</div>
             </div>
-            <img src="https://ic.pics.livejournal.com/anndr_art/18848504/324640/324640_original.jpg" alt=""/>
+            <div className={style.image}>
+              <img src={props.img} alt=''/>
+            </div>
+            <div className={style.text}>
+                {props.message}
+            </div>
         </div>
     )
 }
