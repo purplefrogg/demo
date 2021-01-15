4481f8a236b703be0e821a0a4ec1c16c8eb3b4df
purplefrogg <purplefroggy23@gmail.com>
2021-01-15 20:35:51 +0500

    20: Navlink

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import style from './Body.module.scss'
 import Messages from './Messages/Messages';
 import Navbar from './Navbar/Navbar';
@@ -6,13 +7,16 @@ import News from './News/News'
 
 const Body = () => {
     return (
-        <div className={style.body}>
+		<BrowserRouter>
+		<div className={style.body}>
 					<Navbar />
 					<div className={style.content}>
-						<Messages />
-						{/* <News /> */}
+						 <Route path='/Messages' component={Messages}/>
+						 <Route path='/News' component={News}/>
 					</div>
 				</div>
+		</BrowserRouter>
+        
     )
 }
 
--- src/components/Body/Navbar/Navbar.jsx
+++ src/components/Body/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import style from './Navbar.module.scss'
 
 const Navbar = () => {
@@ -6,22 +7,16 @@ const Navbar = () => {
 		<div className={style.navbar}>
 			<div className={style.navbar__list}>
 				<div className={style.navbar__item}>
-
-					<div className={style.navbar__label}>News
-            </div>
+					<NavLink to='News' activeClassName={style.active} className={style.navbar__label}>News</NavLink>
 				</div>
 				<div className={style.navbar__item}>
-					<div className={style.navbar__label}>messenges
-            </div>
+					<NavLink to='Messages' activeClassName={style.active} className={style.navbar__label}>Messages</NavLink>
 				</div>
 				<div className={style.navbar__item}>
-
-					<div className={style.navbar__label}>friends
-            </div>
+					<NavLink to='friends' className={style.navbar__label}>friends</NavLink>
 				</div>
 				<div className={style.navbar__item}>
-					<div className={style.navbar__label}>profile
-            </div>
+					<NavLink to='profile' className={style.navbar__label}>profile</NavLink>
 				</div>
 			</div>
 		</div>
