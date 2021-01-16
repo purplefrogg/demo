55385d514767e87298adae4f68b72d37b39b64a6
purplefrogg <purplefroggy23@gmail.com>
2021-01-16 10:39:16 +0500

    21: massages grid

--- src/components/Body/Messages/Messages.jsx
+++ src/components/Body/Messages/Messages.jsx
@@ -4,13 +4,16 @@ import style from './Messages.module.scss'
 
 const Messages = () => {
 	return (
-		<div className={style.body}>
-			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, pariatur quo? Ab quaerat dolor architecto, soluta dolore nulla ea. Assumenda voluptate odit fugit? Voluptatibus eos adipisci id ad iste vel.
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed fuga aperiam nesciunt provident aut repellendus saepe hic commodi, doloribus officiis distinctio accusamus ratione suscipit quo earum. Vitae sit sequi amet!
-			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem omnis harum placeat dolorum accusantium beatae error tempora, excepturi a perspiciatis aperiam? Aperiam ullam rerum, provident voluptate praesentium accusamus sapiente qui!
-			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem obcaecati possimus, ab non neque iusto suscipit officia pariatur inventore deserunt voluptatem eos vel reiciendis ipsum explicabo rerum mollitia ea quo!
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem tempora excepturi eveniet ipsam, officia sit deserunt asperiores itaque nemo iste repudiandae facilis sunt exercitationem, hic vero, voluptate veniam porro nesciunt!
-			
+		<div className={style.mainPage}>
+			<div className={style.chats}>
+				<div className={style.chat}>Dio</div>
+				<div className={style.chat +' '+ style.active}>Jojo</div>
+				<div className={style.chat}>Polnareff</div>
+			</div>
+			<div className={style.messages}>
+				<div className={style.message}>who are you</div>
+				<div className={style.message}>I am DIO BRANDO</div>
+			</div>
 		</div>
 	)
 }
