5c50b3d04460eb0f8fd2c38ed0b6d50e9c4af9ff
purplefrogg <purplefroggy23@gmail.com>
2021-01-16 15:46:55 +0500

    25: map

--- src/components/Body/Messages/Messages.jsx
+++ src/components/Body/Messages/Messages.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import style from './Messages.module.scss'
 
+const Chat = (props) => {
+	let path = '/Messages/' + props.id
+	return (
+	<NavLink to={path} activeClassName={style.active} className={style.chat}>{props.name}</NavLink>
+	)
+}
+
+const Message = (props) => {
+	return (
+	<div className={style.message}>{props.message}</div>
+	)
+}
 
 const Messages = () => {
+
+	let chats = [
+		{id: 1 , name:'DIO'},
+		{id: 2, name:'Jojo'},
+		{id: 3, name:'Polnareff'},
+	]
+	let messages = [
+		{message:'who are you'},
+		{message:'I am DIO BRANDO'},
+		
+	]
+	let ChatItems = chats.map(chat =><Chat name={chat.name} id={chat.id}/>)
+	let Messages = messages.map(mess =><Message message={mess.message}/>)
+
 	return (
 		<div className={style.mainPage}>
 			<div className={style.chats}>
-				<div className={style.chat}>Dio</div>
-				<div className={style.chat +' '+ style.active}>Jojo</div>
-				<div className={style.chat}>Polnareff</div>
+				{ChatItems}
 			</div>
 			<div className={style.messages}>
-				<div className={style.message}>who are you</div>
-				<div className={style.message}>I am DIO BRANDO</div>
+				{Messages}
 			</div>
 		</div>
 	)
--- src/components/Body/Navbar/Navbar.jsx
+++ src/components/Body/Navbar/Navbar.jsx
@@ -7,16 +7,16 @@ const Navbar = () => {
 		<div className={style.navbar}>
 			<div className={style.navbar__list}>
 				<div className={style.navbar__item}>
-					<NavLink to='News' activeClassName={style.active} className={style.navbar__label}>News</NavLink>
+					<NavLink to='/News' activeClassName={style.active} className={style.navbar__label}>News</NavLink>
 				</div>
 				<div className={style.navbar__item}>
-					<NavLink to='Messages' activeClassName={style.active} className={style.navbar__label}>Messages</NavLink>
+					<NavLink to='/Messages' activeClassName={style.active} className={style.navbar__label}>Messages</NavLink>
 				</div>
 				<div className={style.navbar__item}>
-					<NavLink to='friends' className={style.navbar__label}>friends</NavLink>
+					<NavLink to='/friends' className={style.navbar__label}>friends</NavLink>
 				</div>
 				<div className={style.navbar__item}>
-					<NavLink to='profile' className={style.navbar__label}>profile</NavLink>
+					<NavLink to='/profile' className={style.navbar__label}>profile</NavLink>
 				</div>
 			</div>
 		</div>
