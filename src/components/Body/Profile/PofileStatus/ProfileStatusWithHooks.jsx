7537c8e296523459b9eed58d9a54234757b9e104
purplefrogg <purplefroggy23@gmail.com>
2021-02-08 16:26:28 +0500

    84: hook, useState

---
+++ src/components/Body/Profile/PofileStatus/ProfileStatusWithHooks.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+
+const ProfileStatusWithHooks = (props) => {
+	let [editMode, setEditMode] = useState(false)
+	let [status, setStatus] = useState(props.status)
+
+
+	const activateEditMode = () => {
+		setEditMode(true)
+	}
+	const deactivateEditMode = () => {
+		setEditMode(false)
+		props.updateStatus(status)
+	}
+	const onStatusChange = (e) => {
+		setStatus(e.currentTarget.value)
+	}
+	return (
+		<div>
+			{!editMode &&
+				<div onClick={activateEditMode}>
+					status: {status}
+				</div>}
+			{editMode &&
+				<div>
+
+					status:
+					<input autoFocus={true} onBlur={deactivateEditMode}
+						onChange={onStatusChange} value={status} />
+				</div>}
+		</div>
+	)
+}
+
+
+export default ProfileStatusWithHooks;
